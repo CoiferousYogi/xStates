fb68a7552e2efaad5c3dfa47c37b17b5ad57b0fc
Ritwik Pai <ritwikpai777@gmail.com>
2024-12-22 13:14:27 +0530

    fixes again

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-//import logo from "./logo.svg";
+import React, { useState, useEffect } from "react";
 import "./App.css";
-import { useState, useEffect } from "react";
 
-function App() {
+const App = () => {
   const [countries, setCountries] = useState([]);
   const [states, setStates] = useState([]);
   const [cities, setCities] = useState([]);
@@ -17,20 +16,12 @@ function App() {
         const response = await fetch(
           "https://crio-location-selector.onrender.com/countries"
         );
-
-        if (!response.ok) {
-          throw new Error(`Error: ${response.status}`);
-        }
-
         const data = await response.json();
-
         setCountries(data);
       } catch (error) {
         console.error("Error fetching countries:", error);
-        setCountries([]);
       }
     };
-
     fetchCountries();
   }, []);
 
@@ -45,12 +36,10 @@ function App() {
         const response = await fetch(
           `https://crio-location-selector.onrender.com/country=${country}/states`
         );
-
         const data = await response.json();
-
         setStates(data);
       } catch (error) {
-        console.error("Error fetching states", error);
+        console.error("Error fetching states:", error);
       }
     } else {
       setStates([]);
@@ -67,67 +56,70 @@ function App() {
         const response = await fetch(
           `https://crio-location-selector.onrender.com/country=${selectedCountry}/state=${state}/cities`
         );
-
         const data = await response.json();
-
         setCities(data);
       } catch (error) {
-        console.error("Error fetching cities", error);
+        console.error("Error fetching cities:", error);
       }
     } else {
       setCities([]);
     }
   };
 
-  const handleCityChange = async (e) => {
-    const city = e.target.value;
-    setSelectedCity(city);
+  const handleCityChange = (e) => {
+    setSelectedCity(e.target.value);
   };
 
   return (
-    <div className="App">
+    <div className="location-selector" style={{ textAlign: "center" }}>
       <h1>Select Location</h1>
-      <br></br>
-      <div className="selectionContainer">
-        <div className="countrySelector">
-          <select value={selectedCountry} onChange={handleCountryChange}>
-            <option value="">Select Country</option>
-            {countries.map((country, index) => (
-              <option key={index} value={country}>
-                {country}
-              </option>
-            ))}
-          </select>
-        </div>
-        <div className="stateSelector">
-          <select value={selectedState} onChange={handleStateChange}>
-            <option value="">Select a State</option>
-            {states.map((state, index) => (
-              <option key={index} value={state}>
-                {state}
-              </option>
-            ))}
-          </select>
-        </div>
-        <div className="citySelector">
-          <select value={selectedCity} onChange={handleCityChange}>
-            <option value="">Select City</option>
-            {cities.map((city, index) => (
-              <option key={index} value={city}>
-                {city}
-              </option>
-            ))}
-          </select>
-        </div>
+      <div>
+        <label></label>
+        <select value={selectedCountry} onChange={handleCountryChange}>
+          <option value="">Select Country</option>
+          {countries.map((country) => (
+            <option key={country} value={country}>
+              {country}
+            </option>
+          ))}
+        </select>
+        &ensp;
+        <select
+          value={selectedState}
+          onChange={handleStateChange}
+          disabled={!selectedCountry}
+        >
+          <option value="">Select State</option>
+          {states.map((state) => (
+            <option key={state} value={state}>
+              {state}
+            </option>
+          ))}
+        </select>
+        &ensp;
+        <select
+          value={selectedCity}
+          onChange={handleCityChange}
+          disabled={!selectedState}
+        >
+          <option value="">Select City</option>
+          {cities.map((city) => (
+            <option key={city} value={city}>
+              {city}
+            </option>
+          ))}
+        </select>
       </div>
-      <br></br>
-      {selectedCountry && selectedState && selectedCity && (
+
+      {selectedCity ? (
         <p>
-          You selected {selectedCity}, {selectedState}, {selectedCountry}
+          You selected <b>{selectedCity}</b>, {selectedState}, {selectedCountry}{" "}
         </p>
+      ) : (
+        <></>
       )}
     </div>
   );
-}
+};
 
 export default App;
